538b146556398d5837961dcba57c219ab96c4178
berluk12@gmail.com <berluk12@gmail.com>
2024-11-10 21:11:25 +0100

    Refactor

--- src/main/java/com/bervan/toolsapp/views/LoginView.java
+++ src/main/java/com/bervan/toolsapp/views/LoginView.java
@@ -1,18 +1,23 @@
 package com.bervan.toolsapp.views;
 
-import com.vaadin.flow.component.html.Div;
+import com.bervan.common.AbstractPageView;
 import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route("login")
 @PageTitle("Login")
-public class LoginView extends Div {
+public class LoginView extends AbstractPageView {
 
     public LoginView() {
+        addClassName("login-page");
         LoginForm loginForm = new LoginForm();
         loginForm.setAction("login");
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignItems(Alignment.CENTER);
+
         add(loginForm);
     }
 }
