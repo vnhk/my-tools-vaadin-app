cb778abb2ee345b7721b0ba63b45cb05b72a8172
berluk12@gmail.com <berluk12@gmail.com>
2025-02-01 00:05:01 +0100

    PWPO-5168 - Added all tasks view

---
+++ src/main/java/com/bervan/toolsapp/views/projectmgmtapp/AllTasksListView.java
@@ -0,0 +1,22 @@
+package com.bervan.toolsapp.views.projectmgmtapp;
+
+import com.bervan.common.service.BaseService;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.projectmgmtapp.model.Task;
+import com.bervan.projectmgmtapp.views.AbstractAllTasksListView;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import jakarta.annotation.security.RolesAllowed;
+
+import java.util.UUID;
+
+@Route(value = AbstractAllTasksListView.ROUTE_NAME, layout = MainLayout.class)
+@RouteAlias(value = AbstractAllTasksListView.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class AllTasksListView extends AbstractAllTasksListView {
+
+    public AllTasksListView(BaseService<UUID, Task> service, BervanLogger log) {
+        super(service, log);
+    }
+}
