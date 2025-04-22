4f0bb862138cbe328c3f38345625b6a8c0426ce2
berluk12@gmail.com <berluk12@gmail.com>
2025-04-22 23:42:44 +0200

    PRSTAT-8010 Best offers view

--- src/main/java/com/bervan/toolsapp/views/shopapp/BestOffersView.java
+++ src/main/java/com/bervan/toolsapp/views/shopapp/BestOffersView.java
@@ -1,17 +1,16 @@
 package com.bervan.toolsapp.views.shopapp;
 
 import com.bervan.core.model.BervanLogger;
-import com.bervan.toolsapp.views.MainLayout;
 import com.bervan.shstat.ProductSearchService;
 import com.bervan.shstat.view.AbstractBestOffersView;
 import com.bervan.shstat.view.DiscountsViewService;
+import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
+import jakarta.annotation.security.RolesAllowed;
 
 @Route(value = AbstractBestOffersView.ROUTE_NAME, layout = MainLayout.class)
-
+@RolesAllowed("USER")
 public class BestOffersView extends AbstractBestOffersView {
-
 
     public BestOffersView(DiscountsViewService discountsViewService, ProductSearchService productSearchService, BervanLogger log) {
         super(discountsViewService, productSearchService, log);
--- src/main/java/com/bervan/toolsapp/views/shopapp/ProductsView.java
+++ src/main/java/com/bervan/toolsapp/views/shopapp/ProductsView.java
@@ -2,16 +2,15 @@ package com.bervan.toolsapp.views.shopapp;
 
 import com.bervan.core.model.BervanLogger;
 import com.bervan.shstat.ProductSearchService;
-import com.bervan.shstat.view.AbstractProductView;
+import com.bervan.shstat.view.AbstractProductsView;
 import com.bervan.shstat.view.ProductViewService;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import jakarta.annotation.security.RolesAllowed;
 
-@Route(value = AbstractProductView.ROUTE_NAME, layout = MainLayout.class)
+@Route(value = AbstractProductsView.ROUTE_NAME, layout = MainLayout.class)
 @RolesAllowed("USER")
-public class ProductsView extends AbstractProductView {
+public class ProductsView extends AbstractProductsView {
 
     public ProductsView(ProductViewService productViewService, ProductSearchService productSearchService, BervanLogger log) {
         super(productViewService, productSearchService, log);
