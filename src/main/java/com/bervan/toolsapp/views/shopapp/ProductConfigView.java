d6c766ecf40071b5c3f1d6ed8f3d4a1b826dd4c1
berluk12@gmail.com <berluk12@gmail.com>
2025-05-11 15:23:39 +0200

    feat: add ProductConfig management view and supporting backend components
    - Introduced ProductConfig entity and JPA mapping
    - Created ProductConfigRepository and ProductConfigService
    - Implemented AbstractProductConfigView with shop filtering and COMMON_USER ownership
    - Added ProductConfigView as a concrete Vaadin route for users

---
+++ src/main/java/com/bervan/toolsapp/views/shopapp/ProductConfigView.java
@@ -0,0 +1,18 @@
+package com.bervan.toolsapp.views.shopapp;
+
+import com.bervan.common.search.SearchService;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.shstat.ProductConfigService;
+import com.bervan.shstat.view.AbstractProductConfigView;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@Route(value = AbstractProductConfigView.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class ProductConfigView extends AbstractProductConfigView {
+
+    public ProductConfigView(ProductConfigService productConfigService, SearchService searchService, BervanLogger log) {
+        super(productConfigService, searchService, log);
+    }
+}
