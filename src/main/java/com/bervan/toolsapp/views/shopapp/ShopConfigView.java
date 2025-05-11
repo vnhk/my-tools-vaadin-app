bbc172b5ca201307c0d2e587e55ff3e3518f43d9
berluk12@gmail.com <berluk12@gmail.com>
2025-05-11 15:24:31 +0200

    feat: Add ShopConfig entity, service, repository, and view
    - Introduced `ShopConfig` entity with fields for shop name, base URL, and products.
    - Created `ShopConfigService` for handling business logic related to ShopConfig.
    - Implemented `ShopConfigRepository` for data access.
    - Developed `ShopConfigView` for UI display and interaction in the Vaadin app.
    - Added `ScrapAudit` entity for tracking product scrap activities.
    - Introduced `VaadinDynamicDropdownTableColumn` annotation for dynamic dropdown support in Vaadin tables.

---
+++ src/main/java/com/bervan/toolsapp/views/shopapp/ShopConfigView.java
@@ -0,0 +1,18 @@
+package com.bervan.toolsapp.views.shopapp;
+
+import com.bervan.common.search.SearchService;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.shstat.ShopConfigService;
+import com.bervan.shstat.view.AbstractShopConfigView;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@Route(value = AbstractShopConfigView.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class ShopConfigView extends AbstractShopConfigView {
+
+    public ShopConfigView(ShopConfigService shopConfigService, SearchService searchService, BervanLogger log) {
+        super(shopConfigService, searchService, log);
+    }
+}
