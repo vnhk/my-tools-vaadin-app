9b8f323978ec64387322788bd9a51c66e77dbaf6
berluk12@gmail.com <berluk12@gmail.com>
2025-05-18 18:52:04 +0200

    Cache clear

--- src/main/java/com/bervan/toolsapp/views/shopapp/BestOffersView.java
+++ src/main/java/com/bervan/toolsapp/views/shopapp/BestOffersView.java
@@ -3,7 +3,7 @@ package com.bervan.toolsapp.views.shopapp;
 import com.bervan.core.model.BervanLogger;
 import com.bervan.shstat.ProductSearchService;
 import com.bervan.shstat.view.AbstractBestOffersView;
-import com.bervan.shstat.view.DiscountsViewService;
+import com.bervan.shstat.DiscountsViewService;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
