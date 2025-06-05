4231bbc8d6e5ccfecc2fd3cd6099113fc78a49e4
berluk12@gmail.com <berluk12@gmail.com>
2025-06-05 20:47:36 +0200

    PRSTAT-5499 - Added price alerts/trackers

---
+++ src/main/java/com/bervan/toolsapp/views/shopapp/ProductAlertsView.java
@@ -0,0 +1,19 @@
+package com.bervan.toolsapp.views.shopapp;
+
+import com.bervan.common.search.SearchService;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.shstat.service.ProductAlertService;
+import com.bervan.shstat.service.ProductConfigService;
+import com.bervan.shstat.view.AbstractProductAlertView;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@Route(value = AbstractProductAlertView.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class ProductAlertsView extends AbstractProductAlertView {
+
+    public ProductAlertsView(ProductAlertService service, SearchService searchService, ProductConfigService productConfigService, BervanLogger log) {
+        super(service, productConfigService, searchService, log);
+    }
+}
