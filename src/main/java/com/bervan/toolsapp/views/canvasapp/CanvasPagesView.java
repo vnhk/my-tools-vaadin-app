2d01b19e10b64d4cdca42242675c4795fa34d92f
berluk12@gmail.com <berluk12@gmail.com>
2025-07-03 21:01:53 +0200

    Canvas/Notepad view layout/styling changed

--- src/main/java/com/bervan/toolsapp/views/canvasapp/CanvasPagesView.java
+++ src/main/java/com/bervan/toolsapp/views/canvasapp/CanvasPagesView.java
@@ -2,18 +2,15 @@ package com.bervan.toolsapp.views.canvasapp;
 
 import com.bervan.streamingapp.CanvasService;
 import com.bervan.streamingapp.view.AbstractCanvasPagesView;
-import com.bervan.core.model.BervanLogger;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import jakarta.annotation.security.RolesAllowed;
 
 @Route(value = AbstractCanvasPagesView.ROUTE_NAME, layout = MainLayout.class)
-
 @RolesAllowed("USER")
 public class CanvasPagesView extends AbstractCanvasPagesView {
 
-    public CanvasPagesView(CanvasService service, BervanLogger log) {
-        super(service, log);
+    public CanvasPagesView(CanvasService service) {
+        super(service);
     }
 }
