1a4804952b17fffd04456b16c93eb810409f802c
berluk12@gmail.com <berluk12@gmail.com>
2025-07-03 11:04:09 +0200

    Styling Options in SpreadsheetView refactored to a new class

--- src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
+++ src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
@@ -1,19 +1,17 @@
 package com.bervan.toolsapp.views.spreadsheetapp;
 
-import com.bervan.spreadsheet.functions.SpreadsheetFunction;
-import com.bervan.spreadsheet.view.AbstractSpreadsheetView;
-import com.bervan.spreadsheet.service.SpreadsheetService;
 import com.bervan.core.model.BervanLogger;
+import com.bervan.spreadsheet.functions.SpreadsheetFunction;
+import com.bervan.spreadsheet.service.SpreadsheetService;
+import com.bervan.spreadsheet.view.AbstractSpreadsheetView;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import jakarta.annotation.security.RolesAllowed;
 
 import java.util.List;
 
 @Route(value = AbstractSpreadsheetView.ROUTE_NAME, layout = MainLayout.class)
-
 @RolesAllowed("USER")
 public class SpreadsheetView extends AbstractSpreadsheetView implements HasUrlParameter<String> {
 
