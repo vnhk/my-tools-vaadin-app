1d6e8f59daa9900066993541f376692b2ba432c1
berluk12@gmail.com <berluk12@gmail.com>
2025-07-19 22:53:14 +0200

    Spreadsheet Styling

--- src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
+++ src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
@@ -1,14 +1,11 @@
 package com.bervan.toolsapp.views.spreadsheetapp;
 
-import com.bervan.spreadsheet.functions.SpreadsheetFunction;
 import com.bervan.spreadsheet.service.SpreadsheetService;
 import com.bervan.spreadsheet.view.AbstractSpreadsheetView;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
-
-import java.util.List;
 
 @Route(value = AbstractSpreadsheetView.ROUTE_NAME, layout = MainLayout.class)
 @RolesAllowed("USER")
