b00979a532e5e65bed50478fc44eef128c52dae7
berluk12@gmail.com <berluk12@gmail.com>
2025-08-15 19:06:14 +0200

    Added context menu for spreadsheet rows

--- src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
+++ src/main/java/com/bervan/toolsapp/views/spreadsheetapp/SpreadsheetView.java
@@ -11,6 +11,9 @@ import jakarta.annotation.security.RolesAllowed;
 @RolesAllowed("USER")
 public class SpreadsheetView extends AbstractSpreadsheetView implements HasUrlParameter<String> {
 
+    // todo
+    //  1. column size adjusting
+    //  2. add context actions for left row header: duplicate row, add row above, delete row, add row below
     public SpreadsheetView(SpreadsheetService service) {
         super(service);
     }
