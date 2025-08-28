1f45ba56445a3ab191055d5b2652832c2729953e
berluk12@gmail.com <berluk12@gmail.com>
2025-08-29 00:29:02 +0200

    Remove WalletTransaction entity and refactor wallet-related functionality
    Eliminated the WalletTransaction entity to simplify the wallet management model. Introduced new constants for wallet-related fields and refined snapshot handling for wallets. Enhanced the WalletView and WalletService to manage wallet snapshots and calculations more effectively.

---
+++ src/main/java/com/bervan/toolsapp/views/investtrackapp/WalletView.java
@@ -0,0 +1,16 @@
+package com.bervan.toolsapp.views.investtrackapp;
+
+import com.bervan.investtrack.service.WalletService;
+import com.bervan.investtrack.service.WalletSnapshotService;
+import com.bervan.investtrack.view.AbstractWalletView;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+@Route(value = AbstractWalletView.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class WalletView extends AbstractWalletView {
+    public WalletView(WalletService service, WalletSnapshotService walletSnapshotService) {
+        super(service, walletSnapshotService);
+    }
+}
