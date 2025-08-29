666e2b0332e45ad0a0369b47582510da0a02ff34
berluk12@gmail.com <berluk12@gmail.com>
2025-08-29 19:00:08 +0200

    Enhance wallet views and validation; improve theme styles
    Added eager loading and ordering to wallet snapshots. Introduced `@NotNull` constraints for critical fields in `WalletSnapshot`. Updated theme styles with primary text color and enhanced error handling across wallet views.

--- src/main/java/com/bervan/toolsapp/views/investtrackapp/WalletBalanceView.java
+++ src/main/java/com/bervan/toolsapp/views/investtrackapp/WalletBalanceView.java
@@ -1,5 +1,6 @@
 package com.bervan.toolsapp.views.investtrackapp;
 
+import com.bervan.investtrack.service.WalletService;
 import com.bervan.investtrack.view.AbstractWalletBalanceView;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
@@ -8,7 +9,7 @@ import jakarta.annotation.security.RolesAllowed;
 @Route(value = AbstractWalletBalanceView.ROUTE_NAME, layout = MainLayout.class)
 @RolesAllowed("USER")
 public class WalletBalanceView extends AbstractWalletBalanceView {
-    public WalletBalanceView() {
-        super();
+    public WalletBalanceView(WalletService walletService) {
+        super(walletService);
     }
 }
