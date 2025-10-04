64f2d1585c8c37b853eae3cd656ed9e1267c0aee
berluk12@gmail.com <berluk12@gmail.com>
2025-10-05 00:09:36 +0200

    Add AsyncTask view and layout for managing async tasks
    Introduce `AbstractAsyncTaskList` as a generic base for async task management with support for common components. Create `AsyncTaskView` to integrate the module into the application, adding a new "Async Tasks" menu item to the main navigation.

---
+++ src/main/java/com/bervan/toolsapp/views/asynctask/AsyncTaskView.java
@@ -0,0 +1,20 @@
+package com.bervan.toolsapp.views.asynctask;
+
+import com.bervan.asynctask.AsyncTask;
+import com.bervan.common.service.BaseService;
+import com.bervan.common.view.AbstractAsyncTaskList;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+import java.util.UUID;
+
+
+@Route(value = AbstractAsyncTaskList.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed({"USER"})
+public class AsyncTaskView extends AbstractAsyncTaskList {
+    public AsyncTaskView(BaseService<UUID, AsyncTask> service, BervanLogger bervanLogger) {
+        super(service, bervanLogger);
+    }
+}
