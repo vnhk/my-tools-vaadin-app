8555dcd37434a6d797118a590e5a058c6e2a7a34
berluk12@gmail.com <berluk12@gmail.com>
2025-10-19 20:03:27 +0200

    Added Interview Home Page

--- src/main/java/com/bervan/toolsapp/views/interview/InterviewHomeView.java
+++ src/main/java/com/bervan/toolsapp/views/interview/InterviewHomeView.java
@@ -1,18 +1,15 @@
 package com.bervan.toolsapp.views.interview;
 
 import com.bervan.interviewapp.view.AbstractInterviewHomeView;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
 import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
 
 @Route(value = AbstractInterviewHomeView.ROUTE_NAME, layout = MainLayout.class)
-
 @RolesAllowed("USER")
 public class InterviewHomeView extends AbstractInterviewHomeView {
 
     public InterviewHomeView() {
         super();
     }
-
 }
