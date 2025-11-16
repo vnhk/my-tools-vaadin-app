feb23a53d3848fff5c67c887b093c8936bcf1afa
berluk12@gmail.com <berluk12@gmail.com>
2025-11-16 19:50:18 +0100

    Added 80 http server

---
+++ src/main/java/com/bervan/toolsapp/security/HttpConnectorConfig.java
@@ -0,0 +1,26 @@
+package com.bervan.toolsapp.security;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.catalina.connector.Connector;
+
+@Configuration
+public class HttpConnectorConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addAdditionalTomcatConnectors(httpConnector());
+        return factory;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        return connector;
+    }
+}
