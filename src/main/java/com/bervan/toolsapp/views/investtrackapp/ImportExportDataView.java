2ed3efeca60ba875f48393495bebb18b63bbbadb
berluk12@gmail.com <berluk12@gmail.com>
2025-11-24 23:21:34 +0100

    Add import-export functionality for WalletSnapshot data
    Introduce `AbstractImportExportData` and `ImportExportDataView` to handle data import/export for WalletSnapshot entities. Updated services, models, and layouts to integrate the feature and enhance logging and notifications for better user feedback.

---
+++ src/main/java/com/bervan/toolsapp/views/investtrackapp/ImportExportDataView.java
@@ -0,0 +1,21 @@
+package com.bervan.toolsapp.views.investtrackapp;
+
+import com.bervan.common.config.BervanViewConfig;
+import com.bervan.common.service.BaseService;
+import com.bervan.core.model.BervanLogger;
+import com.bervan.investtrack.model.WalletSnapshot;
+import com.bervan.investtrack.view.AbstractImportExportData;
+import com.bervan.toolsapp.views.MainLayout;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+
+import java.util.UUID;
+
+@Route(value = AbstractImportExportData.ROUTE_NAME, layout = MainLayout.class)
+@RolesAllowed("USER")
+public class ImportExportDataView extends AbstractImportExportData {
+
+    public ImportExportDataView(BaseService<UUID, WalletSnapshot> dataService, BervanViewConfig bervanViewConfig, BervanLogger logger) {
+        super(dataService, bervanViewConfig, logger);
+    }
+}
