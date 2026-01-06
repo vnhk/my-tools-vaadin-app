c5122495e3e926543d7921f55e1dc7e90c8990a3
berluk12@gmail.com <berluk12@gmail.com>
2026-01-06 23:15:52 +0100

    Streaming Config reload

--- src/main/java/com/bervan/toolsapp/views/streamingplatformapp/ProductionListView.java
+++ src/main/java/com/bervan/toolsapp/views/streamingplatformapp/ProductionListView.java
@@ -2,6 +2,7 @@ package com.bervan.toolsapp.views.streamingplatformapp;
 
 import com.bervan.streamingapp.VideoManager;
 import com.bervan.streamingapp.config.ProductionData;
+import com.bervan.streamingapp.config.StreamingConfigLoader;
 import com.bervan.streamingapp.view.AbstractProductionListView;
 import com.bervan.toolsapp.views.MainLayout;
 import com.vaadin.flow.router.Route;
@@ -13,7 +14,7 @@ import java.util.Map;
 @RolesAllowed({"USER", "STREAMING"})
 public class ProductionListView extends AbstractProductionListView {
 
-    public ProductionListView(Map<String, ProductionData> streamingProductionData, VideoManager videoManager) {
-        super(videoManager, streamingProductionData);
+    public ProductionListView(Map<String, ProductionData> streamingProductionData, VideoManager videoManager, StreamingConfigLoader streamingConfigLoader) {
+        super(videoManager, streamingProductionData, streamingConfigLoader);
     }
 }
