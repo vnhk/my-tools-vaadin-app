920b514652f3d27842611569af3bbe64e45d028b
berluk12@gmail.com <berluk12@gmail.com>
2026-01-26 20:50:03 +0100

    Refactor budget management to use dynamic data model
    Replaced static hardcoded budget categories with a dynamic, database-driven structure. Introduced new BudgetEntry entities, services, repository, and auto-configuration. Updated tree grid to populate data dynamically and added UI support for creating and displaying budget entries with grouping and filtering.

--- src/main/java/com/bervan/toolsapp/views/BudgetView.java
+++ src/main/java/com/bervan/toolsapp/views/BudgetView.java
@@ -2,13 +2,14 @@ package com.bervan.toolsapp.views;
 
 import com.bervan.budget.AbstractBudgetView;
 import com.bervan.budget.BudgetService;
+import com.bervan.common.config.BervanViewConfig;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
 
 @Route(value = "/budget", layout = MainLayout.class)
 @RolesAllowed("USER")
 public class BudgetView extends AbstractBudgetView {
-    public BudgetView(BudgetService budgetService) {
-        super(budgetService);
+    public BudgetView(BudgetService service, BervanViewConfig bervanViewConfig) {
+        super(service, bervanViewConfig);
     }
 }
