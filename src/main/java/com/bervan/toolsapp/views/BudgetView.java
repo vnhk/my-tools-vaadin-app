7e16fa9554474ba522e66f618e8e439aff8c7890
berluk12@gmail.com <berluk12@gmail.com>
2026-01-27 01:06:17 +0100

    Add SaveItemDialog and enhance BudgetGridView with edit and copy features
    Introduced `SaveItemDialog` for reusable saving dialogs and expanded `BudgetGridView` functionality with item copy and edit capabilities. Refactored toolbar to manage actions dynamically, improved UX with streamlined dialog handling, and ensured better modularity through component usage updates.

--- src/main/java/com/bervan/toolsapp/views/BudgetView.java
+++ src/main/java/com/bervan/toolsapp/views/BudgetView.java
@@ -2,6 +2,7 @@ package com.bervan.toolsapp.views;
 
 import com.bervan.budget.AbstractBudgetView;
 import com.bervan.budget.BudgetService;
+import com.bervan.budget.entry.BudgetEntryService;
 import com.bervan.common.config.BervanViewConfig;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
@@ -9,7 +10,8 @@ import jakarta.annotation.security.RolesAllowed;
 @Route(value = AbstractBudgetView.ROUTE_NAME, layout = MainLayout.class)
 @RolesAllowed("USER")
 public class BudgetView extends AbstractBudgetView {
-    public BudgetView(BudgetService service, BervanViewConfig bervanViewConfig) {
-        super(service, bervanViewConfig);
+
+    public BudgetView(BudgetService service, BervanViewConfig bervanViewConfig, BudgetEntryService budgetEntryService) {
+        super(service, bervanViewConfig, budgetEntryService);
     }
 }
